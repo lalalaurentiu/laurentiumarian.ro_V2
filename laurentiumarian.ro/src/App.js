308b2140a1a77a893c101cae8d6e4fcc100514a8
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-08-14 00:40:31 +0300

    add css for App container

--- laurentiumarian.ro/src/App.js
+++ laurentiumarian.ro/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
-import intro from './components/intro';
+import "./App.css";
+import intro from "./components/intro";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="
+      App 
+      bg-[url('../public/images/Desktop/Wallpapers/Wallpaper_mobile.webp')] 
+      md:bg-[url('../public/images/Desktop/Wallpapers/Wallpaper_tablet.webp')] 
+      lg:bg-[url('../public/images/Desktop/Wallpapers/Wallpaper.webp')] 
+      bg-cover 
+      w-screen 
+      h-screen
+    "
+    >
       {intro()}
     </div>
   );
