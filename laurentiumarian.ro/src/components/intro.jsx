0f7788a0381cb6193b4668352a9ccfe9bfad36c4
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-08-14 00:03:49 +0300

    create intro component

---
+++ laurentiumarian.ro/src/components/intro.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+
+export default function Intro() {
+  const [loadingVisible, setLoadingVisible] = useState(true);
+
+  useEffect(() => {
+    const loadingElement = document.querySelector(".loading");
+
+    const handleAnimationEnd = () => {
+      setLoadingVisible(false);
+    };
+
+    if (loadingElement) {
+      loadingElement.addEventListener("animationend", handleAnimationEnd);
+    }
+
+    return () => {
+      if (loadingElement) {
+        loadingElement.removeEventListener("animationend", handleAnimationEnd);
+      }
+    };
+  }, []);
+
+  return (
+    <section
+      id="intro"
+      className={`bg-black w-screen h-screen text-white ${
+        loadingVisible ? "" : "hidden"
+      }`}
+    >
+      <div className="flex items-center justify-center flex-col h-full">
+        <h1 className="lg:text-7xl md:text-5xl sm:text-3xl text-xl font-black">
+          Laurentiu Marian
+        </h1>
+        <div className="w-9/12 lg:h-5 md:h-3 h-2 mt-1">
+          <div className="loading bg-white rounded-full w-full h-full"></div>
+        </div>
+      </div>
+    </section>
+  );
+}
