f6f403f570e0dffe2867d966f4a3bd028052b621
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-08-14 20:27:26 +0300

    add navbar in App.js

--- laurentiumarian.ro/src/App.js
+++ laurentiumarian.ro/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import intro from "./components/intro";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <div
       className="
       App 
-      bg-[url('../public/images/Desktop/Wallpapers/Wallpaper_mobile.webp')] 
-      md:bg-[url('../public/images/Desktop/Wallpapers/Wallpaper_tablet.webp')] 
+      bg-[url('../public/images/Mobile/Wallpapers/Wallpaper_mobile.webp')] 
+      md:bg-[url('../public/images/Tablet/Wallpapers/Wallpaper_tablet.webp')] 
       lg:bg-[url('../public/images/Desktop/Wallpapers/Wallpaper.webp')] 
       bg-cover 
       w-screen 
@@ -15,6 +16,7 @@ function App() {
     "
     >
       {intro()}
+      {Navbar()}
     </div>
   );
 }
